c035fc442bbbbae0115cbe823ecf6ed4ce5973cc
Klay2 <klaykrueger@wisc.edu>
2016-04-21 22:17:11 -0500

    some adjustments pending to get promo item on main activity

--- app/src/main/java/com/example/yeonjun/walkingdistance/MainActivity.java
+++ app/src/main/java/com/example/yeonjun/walkingdistance/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,6 +21,10 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -134,5 +141,34 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
 
+    }
+
+    private class DownloadPromo extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            //TODO:  USE COORDINATES TO GET NEW PROMO and add it to the arraylist and update adapter
+             final OkHttpClient client = new OkHttpClient();
+             final Gson gson = new Gson();
+
+
+                Request request = new Request.Builder()
+                        .url("https://api.github.com/gists/c2a7c39532239ff261be")
+                        .build();
+            Response response = null;
+
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                PromObject promTemp = gson.fromJson(response.body().charStream(), PromObject.class);
+            /*
+                for (Map.Entry<String, GistFile> entry : gist.files.entrySet()) {
+                    System.out.println(entry.getKey());
+                    System.out.println(entry.getValue().content);
+                }*/
+
+
+
+            return null;
+        }
     }
 }
