cdf81560d5ca0641264be2f1afdc2660aabda1ee
Grisha <Kamyshnikov@wisc.edu>
2016-04-25 14:21:00 -0500

    Added Google Maps. Added Liked Gridview.

--- app/src/main/java/com/example/yeonjun/walkingdistance/MainActivity.java
+++ app/src/main/java/com/example/yeonjun/walkingdistance/MainActivity.java
@@ -1,30 +1,23 @@
 package com.example.yeonjun.walkingdistance;
 
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageInstaller;
-import android.content.pm.PackageManager;
-import android.nfc.Tag;
+
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ListView;
+
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.games.GamesMetadata;
-import com.google.android.gms.gcm.GoogleCloudMessaging;
-import com.google.gson.Gson;
+
 import com.googlecode.objectify.ObjectifyService;
 
 import java.io.File;
@@ -36,6 +29,9 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -44,13 +40,12 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<PromObject> promotions;
     private ArrayList<PromObject> likedPromotions;
-    private GridView promoListview ;
-    private CustomPromoAdapter mPromoAdapter ;
+    private GridView promoListview;
+    private CustomPromoAdapter mPromoAdapter;
     private BroadcastReceiver reloadPromoReceiver;
     private IntentFilter iFilter;
-    Button Liked ;
-    Button allPromos ;
-
+    Button Liked;
+    Button allPromos;
 
 
     @Override
@@ -66,58 +61,58 @@ public class MainActivity extends AppCompatActivity {
 
 
         // 2promotions.add();
-        promoListview = (GridView)findViewById(R.id.gridView);
-        mPromoAdapter = new CustomPromoAdapter(this, R.layout.list_promobject_layout,promotions);
+        promoListview = (GridView) findViewById(R.id.gridView);
+        mPromoAdapter = new CustomPromoAdapter(this, R.layout.list_promobject_layout, promotions);
 
 
         iFilter = new IntentFilter();
         iFilter.addAction("reloadNewPromos");
-        Intent serviceIntent = new Intent(this,GPSPromoGetService.class);
+        Intent serviceIntent = new Intent(this, GPSPromoGetService.class);
         startService(serviceIntent);
-        Toast.makeText(getApplicationContext(), "getting this far",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "getting this far", Toast.LENGTH_SHORT).show();
 
         //Stufff done for receiving broadcast from the background intent service
-        reloadPromoReceiver = new BroadcastReceiver(){
+        reloadPromoReceiver = new BroadcastReceiver() {
             public void onReceive(Context context, Intent intent) {
                 //loadFromDisk();//TODO:this should be done async
                 //mPromoAdapter.notifyDataSetChanged();
-                Toast.makeText(getApplicationContext(), "got Latitude: " + intent.getDoubleExtra("Latitude",0),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "got Latitude: " + intent.getDoubleExtra("Latitude", 0), Toast.LENGTH_SHORT).show();
 
             }
         };
 
-        registerReceiver(reloadPromoReceiver,iFilter);
+        registerReceiver(reloadPromoReceiver, iFilter);
 
 
-        if(promoListview != null){
+        if (promoListview != null) {
             promoListview.setAdapter(mPromoAdapter);
         }
 
         Liked.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mPromoAdapter = new CustomPromoAdapter(MainActivity.this, R.layout.list_promobject_layout,likedPromotions);
+                mPromoAdapter = new CustomPromoAdapter(MainActivity.this, R.layout.list_promobject_layout, likedPromotions);
             }
         });
         allPromos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mPromoAdapter = new CustomPromoAdapter(MainActivity.this, R.layout.list_promobject_layout,promotions);
+                mPromoAdapter = new CustomPromoAdapter(MainActivity.this, R.layout.list_promobject_layout, promotions);
             }
         });
 
         promoListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    PromObject Obj = (PromObject) parent.getAdapter().getItem(position);
-                    Intent mIntent = new Intent(MainActivity.this , toPromoImage.class);
-                    Bundle mBundle = new Bundle();
-                    mBundle.putDouble("Long", Obj.getLongitude());
-                    mBundle.putDouble("Lat", Obj.getLatitude());
-                    mBundle.putString("ImageUrl", Obj.getbusinessPhoto());
-                    mBundle.putString("Bname", Obj.getBusinessName());
-                    mBundle.putString("Exp", Obj.promoDateTime());
-                    mIntent.putExtras(mBundle);
+                PromObject Obj = (PromObject) parent.getAdapter().getItem(position);
+                Intent mIntent = new Intent(MainActivity.this, toPromoImage.class);
+                Bundle mBundle = new Bundle();
+                mBundle.putDouble("Long", Obj.getLongitude());
+                mBundle.putDouble("Lat", Obj.getLatitude());
+                mBundle.putString("ImageUrl", Obj.getbusinessPhoto());
+                mBundle.putString("Bname", Obj.getBusinessName());
+                mBundle.putString("Exp", Obj.promoDateTime());
+                mIntent.putExtras(mBundle);
 
 
             }
@@ -128,41 +123,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 PromObject likedObj = (PromObject) parent.getAdapter().getItem(position);
-                if (likedPromotions.isEmpty()){
+                if (likedPromotions.isEmpty()) {
                     likedPromotions.add(likedObj);
-                }
-                else {
-                    int i = 0 ;
+                } else {
+                    int i = 0;
                     boolean contains = false;
-                    while (i < likedPromotions.size() && contains ){
-                        if (likedObj.getBusinessName().equals( likedPromotions.get(i).getBusinessName())){
-                        contains = true ;
+                    while (i < likedPromotions.size() && contains) {
+                        if (likedObj.getBusinessName().equals(likedPromotions.get(i).getBusinessName())) {
+                            contains = true;
                         }
-                        i++ ;
+                        i++;
                     }
-                    if(contains == false){
+                    if (contains == false) {
                         likedPromotions.add(likedObj);
-                        Toast.makeText(MainActivity.this, "Added To Liked",Toast.LENGTH_SHORT);
+                        Toast.makeText(MainActivity.this, "Added To Liked", Toast.LENGTH_SHORT);
 
                     }
                 }
 
 
-
-                return true ;
+                return true;
             }
         });
-
 
 
         boolean loadResult;
         promotions = null;
         loadResult = loadFromDisk();
-        if(loadResult  && promotions != null){
+        if (loadResult && promotions != null) {
             //load successful, TODO:check for and delete expired promos finish the method via getting an iterator etc
             deleteExpired(promotions);
         }
-
 
 
     }
@@ -170,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
     //method for combing over promotions in the array and deleting
     //them if  they are currently expired
     //TODO: finish this thingy
-    private void deleteExpired(ArrayList<PromObject> promotionsIn){
+    private void deleteExpired(ArrayList<PromObject> promotionsIn) {
 
         return;
     }
@@ -179,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
     //returns a boolean for whether it failed or not
     //call this whenever the activity is about to end
     //TODO: sloppily done, needs some more advanced checking or whatever maybe
-    private boolean saveToDisk(){
+    private boolean saveToDisk() {
         File fileOut = new File(this.getFilesDir(), "promotions.sv");
         FileOutputStream fileOutStream;
         ObjectOutputStream objectsOut;
@@ -191,11 +182,10 @@ public class MainActivity extends AppCompatActivity {
             fileOutStream.close();
 
 
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
             return false;
-        }
-        catch(IOException f){
+        } catch (IOException f) {
             f.printStackTrace();
             return false;
         }
@@ -206,23 +196,22 @@ public class MainActivity extends AppCompatActivity {
     //method for loading promotions from disk
     //returns a boolean for failure etc
     //TODO:  sloppily done needs some more advanced file checking or whatever maybe
-    private boolean loadFromDisk(){
+    private boolean loadFromDisk() {
         FileInputStream fileInStream;
         ObjectInputStream objectsIn;
-        try{
+        try {
             fileInStream = openFileInput("promotions.sv");
             objectsIn = new ObjectInputStream(fileInStream);
-            promotions = (ArrayList<PromObject>)objectsIn.readObject();
+            promotions = (ArrayList<PromObject>) objectsIn.readObject();
             fileInStream.close();
             objectsIn.close();
-            if(promotions == null){
+            if (promotions == null) {
                 return false;
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
-        }
-        catch(ClassNotFoundException f){
+        } catch (ClassNotFoundException f) {
             f.printStackTrace();
             return false;
         }
@@ -230,18 +219,49 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private class DownloadPromo extends AsyncTask<String,Void,String> {
+    private class DownloadPromo extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... params) {
             //TODO:  USE COORDINATES TO GET NEW PROMO and add it to the arraylist and update adapter
 
+            OkHttpClient client = new OkHttpClient();
 
+            intent.getDoubleExtra("Latitude", 0)
 
+            client.run();
+        }
 
-            return null;
+        public void run() throws Exception {
+            Request request = new Request.Builder()
+                    .url("http://hidden-fortress-95984.heroku.com/db")
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
+
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+
+                    System.out.println(response.body().string());
+                }
+            });
         }
     }
+}
+
+
+
 
     private class OrmStartup extends AsyncTask<String,Void,String> {
 
